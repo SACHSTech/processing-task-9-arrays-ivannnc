3a2e12105a4441a7d7f2bacae696c584855f2e93
ivannnc <junxian.chen24@ycdsbk12.ca>
2023-12-20 16:04:30 -0500

    submit

--- Sketch.java
+++ Sketch.java
@@ -3,9 +3,17 @@ import processing.core.PApplet;
 public class Sketch extends PApplet {
 	
 	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  int numSnowflakes = 100;
+  float[] snowflakesX;
+  float[] snowflakesY;
+  float[] snowflakesSpeed;
+
+  float playerX;
+  float playerY;
+  int playerLives = 3;
+
+  boolean[] ballHideStatus;
+
   public void settings() {
 	// put your size call here
     size(400, 400);
@@ -17,20 +25,107 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
-  }
+    snowflakesX = new float[numSnowflakes];
+        snowflakesY = new float[numSnowflakes];
+        snowflakesSpeed = new float[numSnowflakes];
+        ballHideStatus = new boolean[numSnowflakes];
+
+        for (int i = 0; i < numSnowflakes; i++) {
+            snowflakesX[i] = random(width);
+            snowflakesY[i] = random(height);
+            snowflakesSpeed[i] = random(1, 3);
+            ballHideStatus[i] = false;
+          }
+
+          playerX = width / 2;
+          playerY = height - 50;
+
+        }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    background(255);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+        moveSnowflakes();
+        drawSnowflakes();
+
+        movePlayer();
+        drawPlayer();
+
+        drawLives();
+  }
+
+  private void drawLives() {
+    fill(255, 0, 0);
+    for (int i = 0; i < playerLives; i++) {
+    rect(width - 30 - i * 40, 10, 30, 30);
+    }
+  }
+
+  private void drawPlayer() {
+    fill(0, 0, 255);
+    ellipse(playerX, playerY, 30, 30);
+  }
+
+  private void movePlayer() {
+    if (keyPressed) {
+      if (keyCode == LEFT) {
+          playerX -= 5;
+      } else if (keyCode == RIGHT) {
+          playerX += 5;
+      }
+  }
+    playerX = constrain(playerX, 0, width - 1);
+  }
+
+  private void drawSnowflakes() {
+    fill(255);
+        stroke(0);
+
+        for (int i = 0; i < numSnowflakes; i++) {
+            if (!ballHideStatus[i]) {
+                ellipse(snowflakesX[i], snowflakesY[i], 10, 10);
+            }
+        }
+  }
+
+  private void moveSnowflakes() {
+    for (int i = 0; i < numSnowflakes; i++) {
+      snowflakesY[i] += snowflakesSpeed[i];
+
+      // Wrap snowflakes around the screen
+      if (snowflakesY[i] > height) {
+          snowflakesY[i] = 0;
+          snowflakesX[i] = random(width);
+      }
+
+      // Check for collision with player
+      if (dist(playerX, playerY, snowflakesX[i], snowflakesY[i]) < 15 && !ballHideStatus[i]) {
+          ballHideStatus[i] = true;
+          playerLives--;
+
+          // Check if player is out of lives
+          if (playerLives <= 0) {
+              gameOver();
+          }
+      }
+  }
+  }
+
+  private void gameOver() {
+    background(255);
+        textSize(36);
+        fill(0);
+        text("Game Over", width / 2, height / 2);
   }
   
-  // define other methods down here.
+  public void mousePressed() {
+    for (int i = 0; i < numSnowflakes; i++) {
+        if (dist(mouseX, mouseY, snowflakesX[i], snowflakesY[i]) < 15) {
+            ballHideStatus[i] = true;
+        }
+    }
+  }
 }
